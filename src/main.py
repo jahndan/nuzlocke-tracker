c655dbf15d0ccc47a2e21e1156ef1dd129c04604
jahndan <jahndandot@gmail.com>
2024-09-28 21:11:25 -0500

    saving/loading tracker state

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 import numpy
 import cv2 as opencv
 from mss import mss
@@ -21,14 +22,6 @@ def print_arg_help():
 
 
 if __name__ == "__main__":
-    if False:  # tracker savefile found
-        # state = model.TrackerState(stuff from savefile)
-        pass
-    else:
-        # initialize a new tracker state assuming a new game
-        state = model.TrackerState(location="Twinleaf Town")
-        # assumed english for now since no other models exist yet
-
     ## TODO replace this with a more flexible way of passing args than sys.argv
     ## also allow user to specify language (currently only English supported)
     # get all necessary args if given
@@ -43,42 +36,56 @@ if __name__ == "__main__":
     # print explanation of necessary args
     except (IndexError, TypeError):
         print_arg_help()
+        exit(1)
     except ValueError:
         print("ERROR: Scale factor must be positive and nonzero!")
+        exit(1)
 
-    # if all args successfully parsed, continue
-    else:
-        sct = mss()
-        bounding_box = {"width": width, "height": height, "left": left, "top": top}
+    # if all args successfully parsed, load tracker savestate and continue
+    try:
+        with open("savestate.pickle", "rb") as load:
+            state = pickle.load(load)
+    except:
+        # initialize a new tracker state assuming a new game
+        state = model.TrackerState(location="Twinleaf Town")
+        # assumed english for now since no other models exist yet
 
-        # initialize the tracker display canvas
-        canvas = numpy.full((384, 256, 3), model.display_palette[0], dtype=numpy.uint8)
-        model.draw_to_display(state, canvas)
-        opencv.imshow("tracker", canvas)
+    sct = mss()
+    bounding_box = {"width": width, "height": height, "left": left, "top": top}
 
-        # temp: solely to make debugging less cluttered
-        prev_state = model.TrackerState()  # invalid state so it prints initially
+    # initialize the tracker display canvas
+    canvas = numpy.full((384, 256, 3), model.display_palette[0], dtype=numpy.uint8)
+    model.draw_to_display(state, canvas)
+    opencv.imshow("tracker", canvas)
 
-        while True:
-            gbra = numpy.array(sct.grab(bounding_box))
-            img = gbra[:, :, :3]  # discard alpha from screen capture
-            res = opencv.resize(
-                img, None, fx=scale, fy=scale, interpolation=opencv.INTER_NEAREST
-            )
+    # temp: solely to make debugging less cluttered
+    prev_state = ""
 
-            model.process_frame(state, res)  # may mutate state
-            if event_queue:  # implicitly evaluates false if empty
-                model.handle_event(state, event_queue.popleft())
+    while True:
+        gbra = numpy.array(sct.grab(bounding_box))
+        img = gbra[:, :, :3]  # discard alpha from screen capture
+        res = opencv.resize(
+            img, None, fx=scale, fy=scale, interpolation=opencv.INTER_NEAREST
+        )
 
-            # if the state has changed, update the display canvas
-            if state.__repr__() != prev_state:
-                prev_state = state.__repr__()
-                print(f"{bold}STATE{reset}")
-                print(debug_format(state))
-                model.draw_to_display(state, canvas)
-                opencv.imshow("tracker", canvas)
+        model.process_frame(state, res)  # may mutate state
+        if event_queue:  # implicitly evaluates false if empty
+            model.handle_event(state, event_queue.popleft())
 
-            opencv.imshow("screen", res)
-            if (opencv.waitKey(1) & 0xFF) == ord("q"):
-                opencv.destroyAllWindows()
-                break
+        # if the state has changed, update the display canvas
+        if state.__repr__() != prev_state:
+            prev_state = state.__repr__()
+            print(f"{bold}STATE{reset}")
+            print(debug_format(state))
+            model.draw_to_display(state, canvas)
+            opencv.imshow("tracker", canvas)
+
+        opencv.imshow("screen", res)  # mainly for checking the bounds were correct
+        if (opencv.waitKey(1) & 0xFF) == ord("q"):
+            opencv.destroyAllWindows()
+            break
+
+    # on exit
+    with open("savestate.pickle", "wb") as write:
+        print("Saving current tracker state to 'savestate.pickle'")
+        pickle.dump(state, write, protocol=pickle.HIGHEST_PROTOCOL)
